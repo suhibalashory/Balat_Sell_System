79022a647c6fa905975bb12e4730710507343f29
suhibalashory <114934177+suhibalashory@users.noreply.github.com>
2025-08-06 21:21:28 +0300

    Add files via upload

---
+++ DataSetting.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace DataLeayrs
+{
+	static class DataSetting
+	{
+		public static string Connaction_string = "Server=.;Database=Alkirat;User Id=sa;Password=123456asd;";
+	}
+}
